38070facb348820f5a041cbc39ccaa68a4b12a27
lipelopeslage <lipelopeslage@hotmail.com>
2018-07-18 08:17:18 -0300

    first commit, hello world

---
+++ index.js
@@ -0,0 +1,35 @@
+var http = require('https');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './app/index.html';
+        console.log('renderizando')
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('rendering...')
